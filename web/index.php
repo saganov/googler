82e93a894e7a5ee6c4d49058a343e11dee53337f
Petr Saganov <saganoff@gmail.com>
2012-11-20 01:30:58 +0200

    Fix the requires

--- web/index.php
+++ web/index.php
@@ -4,9 +4,9 @@ $query  = isset($_REQUEST['q']) ? $_REQUEST['q'] : NULL;
 $source = isset($_REQUEST['s']) ? $_REQUEST['s'] : NULL;
 $page   = isset($_REQUEST['p']) ? $_REQUEST['p'] : 0;
 
-require_once dirname(__DIR__) ."/app/App.php";
-require_once dirname(__DIR__)."/src/View.php";
-
+require_once (dirname(__DIR__) ."/app/App.php");
+require_once (dirname(__DIR__)."/src/View.php");
+                                                           
 View::setTemplateDir(dirname(__DIR__) ."/view/web");
 
 $app = new App();
