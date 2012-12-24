ed25285d381687ef0a7552c6697663cf1220f46d
Pyotr Saganov <pyotr.saganov@toatech.com>
2012-12-24 12:47:15 +0200

    Add the description for the embeded video

--- view/web/embed_youtube.html.php
+++ view/web/embed_youtube.html.php
@@ -2,7 +2,8 @@
 <br/>
 <h3><?= $item['title'] ?></h3>
 <iframe width="420" height="315" src="http://www.youtube.com/embed/<?= $item['url'] ?>" frameborder="0" allowfullscreen></iframe>
-<div>
+<div style="float:left"><?= $item['description'] ?></div>
+<div style="clear:left">
 <?php foreach($related as $video): ?>
 <div style="margin:5px;float:left">
   <a href="<?= 'index.php?m=embedYoutube&url='.$video['url'] ?>"><img
